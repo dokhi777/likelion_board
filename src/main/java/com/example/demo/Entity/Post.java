b83870df337b0894e0c37b5a43853690b1fdf4d8
dokhi777 <parkjane0717@gmail.com>
2025-05-08 19:42:16 +0900

    Feat: User, Post, Comment 엔티티 생성 및 연관관계 설정

---
+++ src/main/java/com/example/demo/Entity/Post.java
@@ -0,0 +1,26 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
